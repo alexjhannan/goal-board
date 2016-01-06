41eb248b72513831f7454525c118f86b141c01f3
dasbuuten <alexjhannan@gmail.com>
2016-01-05 20:26:43 -0500

    added functionality to rewards page. app is now ready for styling.

--- app/scripts/controllers/rewards.js
+++ app/scripts/controllers/rewards.js
@@ -9,12 +9,16 @@
  */
 angular.module('yeomanTutorialApp')
   .controller('RewardsCtrl', function ($scope, localStorageService) {
-    $scope.completes = localStorageService.get('completes') || [];
+    var localStoreCompletes = localStorageService.get('completes') || [];
     
-    // both rewards and purchased have their own array in local storage
-    $scope.rewards = localStorageService.get('rewards') || [];
+    // both unpurchased and purchased have their own array in local storage
+    var localStoreUnpurchased = localStorageService.get('unpurchased') || [];
     
-    $scope.purchased = localStorageService.get('purchased') || [];
+    var localStorePurchased = localStorageService.get('purchased') || [];
+    
+    $scope.completes = localStoreCompletes || [];
+    $scope.unpurchased = localStoreUnpurchased || [];
+    $scope.purchased = localStorePurchased || [];
     
     // should be refactored into a service...
     $scope.countCompletions = function(){
@@ -29,11 +33,66 @@ angular.module('yeomanTutorialApp')
     $scope.countSpent = function(){
         var spent = 0;
         $scope.purchased.forEach(function(el){
-            spent += el.pomos;
+            spent += el.cost;
         });
         
         return spent;
     };
+    
+    $scope.addUnpurchased = function(){
+        if ($scope.reward.name){
+            // fix cases where cost may not work
+            if (!$scope.reward.cost || $scope.reward.cost < 0){
+                $scope.reward.cost = 0;
+            }
+            
+            if (!$scope.reward.url){
+                $scope.reward.url = '#/rewards';
+            }
+            
+            $scope.unpurchased.push($scope.reward);
+            
+            $scope.reward = {};
+        }
+        
+    };
+    
+    // remove unpurchased reward without passing it to purchased
+    $scope.removeUnpurchased = function(index){
+        $scope.unpurchased.splice(index, 1);
+    };
+    
+    // remove purchased reward (will change point count)
+    $scope.removePurchased = function(index){
+        $scope.purchased.splice(index, 1);
+    };
+    
+    // 'purchase' a reward by sending it to the purchased array
+    $scope.purchaseUnpurchased = function(index){
+        if ($scope.count - $scope.spent > $scope.unpurchased[index].cost){
+        
+            var reward = $scope.unpurchased.splice(index, 1)[0];
+        
+            $scope.purchased.push(reward);
+            
+            // update counts
+            $scope.count = $scope.countCompletions();
+            $scope.spent = $scope.countSpent();
+        }
+        else{
+            // add in a failure animation here
+            console.log('fail');
+        }
+    };
+    
+    // watchers for purch & unpurch arrays
+    $scope.$watch('unpurchased', function(){
+      localStorageService.set('unpurchased', $scope.unpurchased);
+    }, true);
+    
+    $scope.$watch('purchased', function(){
+      localStorageService.set('purchased', $scope.purchased);
+    }, true);
     
     $scope.count = $scope.countCompletions();
     $scope.spent = $scope.countSpent();
